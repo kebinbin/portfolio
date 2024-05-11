730cd95757ac91c92fd6cdf17a88b620f584bfc9
kevin.castillo <kevin.castillo@upr.edu>
2024-05-10 21:37:37 -0400

    Initial commit with general project structure

---
+++ src/i18n/ui.ts
@@ -0,0 +1,22 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.brand": "Kevin Castillo",
+    "nav.projects": "Projects",
+    "nav.about": "About",
+    "nav.contact": "Contact",
+    "nav.lang": "ES",
+  },
+  es: {
+    "nav.projects": "Proyectos",
+    "nav.about": "Acerca",
+    "nav.contact": "Contacto",
+    "nav.lang": "EN",
+  },
+} as const;
