d4d5a0e4baf0ff0cbf0f3bc622201a416dff2d40
kevin.castillo <kevin.castillo@upr.edu>
2025-02-26 17:07:48 -0300

    Upgraded tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+// import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  i18n: {
-    defaultLocale: "en",
-    locales: ["es", "en"]
-    // routing: {
-    //   prefixDefaultLocale: false,
-    // },
-  },
-  integrations: [tailwind()]
-});
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['es', 'en']
+		// routing: {
+		//   prefixDefaultLocale: false,
+		// },
+	},
+	integrations: [],
+	vite: {
+		plugins: [tailwindcss()]
+	}
+})
